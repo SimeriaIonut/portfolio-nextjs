f4c758335cff874236dc2699a8bdc193ede3afcb
Sergiu-Ionut Simeria <SERS@danskebank.dk>
2021-01-01 13:02:55 +0100

    feat: added scale from origin feature

--- components/GridItem/index.js
+++ components/GridItem/index.js
@@ -3,19 +3,26 @@ import FullPage from '../FullPage'
 import styles from './gridItem.module.scss';
 
 export default function GridItem({ project }) {
-  const [showFullPage, setShowFullPage] = useState(false);
+  const [showFullPage, setShowFullPage] = useState(null);
+
+  const handleGridItemClick = (e) => {
+    const gridItem = e.target.getBoundingClientRect();
+    
+    const centerX = gridItem.x + (gridItem.width / 2);
+    const centerY = gridItem.y + (gridItem.height / 2);
+
+    setShowFullPage({ centerX, centerY });
+  }
 
   return (
     <>
-      <div className={styles.gridItem} onClick={() => setShowFullPage(true)}>
+      <div className={styles.gridItem} onClick={handleGridItemClick}>
         <img className={styles.gridItemImage} src={project.image} alt="project-image" />
         <div className={styles.seeMoreContainer}>
           <div className={styles.seeMore}>See more</div>
         </div>
       </div>
-      {showFullPage && (
-        <FullPage closeFullPage={() => setShowFullPage(false)} project={project} />
-      )}
+      <FullPage showFullPage={showFullPage} closeFullPage={() => setShowFullPage(false)} project={project} />
     </>
   )
 }
